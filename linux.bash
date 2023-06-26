5c33199a9bd391ef5a31b1dad20c62fa08b509a0
keyhackers <50827272+keyhacks@users.noreply.github.com>
2023-06-26 13:18:04 +0200

    Update linux.bash

--- linux.bash
+++ linux.bash
@@ -9,28 +9,31 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install apache2 -y
 
 # Install MySQL
-echo "mysql-server mysql-server/root_password password yourpassword" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password yourpassword" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password password Welkom01!" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password Welkom01!" | sudo debconf-set-selections
 sudo apt install mysql-server -y
 
 # Install PHP
-sudo apt install php libapache2-mod-php php-mysql -y
+sudo apt install php libapache2-mod-php php-mysql php-imagick php-dompdf php-xml php-mbstring php-gd php-pdo php-json php-curl php-zip php-gmp php-bcmath -y
+
+# Enable Apache mod_rewrite
+sudo a2enmod rewrite
+
+# Restart Apache service
+sudo systemctl restart apache2
 
 # Install phpMyAdmin
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | sudo debconf-set-selections
-echo "phpmyadmin phpmyadmin/app-password-confirm password yourpassword" | sudo debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/admin-pass password yourpassword" | sudo debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/app-pass password yourpassword" | sudo debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password Welkom01!" | sudo debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password Welkom01!" | sudo debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password Welkom01!" | sudo debconf-set-selections
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | sudo debconf-set-selections
 sudo apt install phpmyadmin -y
 
 # Owncloud installation
-# Install dependencies
-sudo apt install -y php7.2-gd php7.2-json php7.2-mysql php7.2-curl php7.2-mbstring php7.2-intl php-imagick php7.2-xml php7.2-zip
-
 # Download and extract Owncloud
-wget https://download.owncloud.org/community/owncloud-complete-20200731.zip
-unzip owncloud-complete-20200731.zip
+wget https://download.owncloud.org/community/owncloud-complete-latest.zip
+unzip owncloud-complete-latest.zip
 sudo mv owncloud /var/www/
 
 # Set appropriate permissions
@@ -38,9 +41,9 @@ sudo chown -R www-data:www-data /var/www/owncloud/
 sudo chmod -R 755 /var/www/owncloud/
 
 # Create database for Owncloud
-mysql -uroot -pyourpassword <<QUERY_INPUT
+mysql -uroot -pWelkom01! <<QUERY_INPUT
 CREATE DATABASE owncloud;
-GRANT ALL ON owncloud.* to 'owncloud'@'localhost' IDENTIFIED BY 'yourpassword';
+GRANT ALL ON owncloud.* to 'owncloud'@'localhost' IDENTIFIED BY 'Welkom01!';
 FLUSH PRIVILEGES;
 QUERY_INPUT
 
@@ -67,7 +70,7 @@ sudo bash -c "cat > /etc/apache2/sites-available/owncloud.conf <<EOF
 EOF"
 
 # Enable site and restart Apache
-sudo a2ensite owncloud
+sudo a2ensite owncloud.conf
 sudo systemctl restart apache2
 
 echo "phpMyAdmin and Owncloud installation completed"
